a94e9ed37f29f5b02667709360ad1a8640765a8c
yashraj-shitole <yashrajs927@gmail.com>
2024-03-19 00:51:30 +0530

    footer

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Profile = () => {
   return (
-    <div className='flex p-5 md:p-10 flex-col h-[80vh]   items-center justify-evenly'>
+    <div className='flex p-5 md:p-10 flex-col h-[100vh]   items-center justify-evenly'>
       <div className='flex  flex-col gap-5 md:gap-20 items-center w-full'>
         <div className='flex overflow-hidden items-center justify-center rounded-full h-[200px] w-[200px]'>
           <img src="https://cdn3d.iconscout.com/3d/premium/thumb/man-avatar-6299539-5187871.png?f=webp" alt="No profile photo" />
