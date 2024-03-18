bcb628ab5f790bf4df23ca61d4981a8104b1d427
yashraj-shitole <yashrajs927@gmail.com>
2024-03-19 00:47:42 +0530

    profile avatar

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -5,7 +5,7 @@ const Profile = () => {
     <div className='flex p-5 md:p-10 flex-col h-[80vh]   items-center justify-evenly'>
       <div className='flex  flex-col gap-5 md:gap-20 items-center w-full'>
         <div className='flex overflow-hidden items-center justify-center rounded-full h-[200px] w-[200px]'>
-          <img src="https://cdn3d.iconscout.com/3d/premium/thumb/man-avatar-6299539-5187871.png?f=webp" alt="" />
+          <img src="https://cdn3d.iconscout.com/3d/premium/thumb/man-avatar-6299539-5187871.png?f=webp" alt="No profile photo" />
         </div>
         <div>
           <h1 className='font-bold text-3xl'>Yashraj Shitole</h1>
